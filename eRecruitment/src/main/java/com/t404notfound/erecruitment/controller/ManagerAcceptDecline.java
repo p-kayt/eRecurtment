ea4e30da86357bc4fc81f7fe2dc3efd5505b33d7
Savoy the Inisolated <chanhduc2002@gmail.com>
2022-11-02 12:18:08 +0700

    edit header.jsp role-based

--- eRecruitment/src/main/java/com/t404notfound/erecruitment/controller/ManagerAcceptDecline.java
+++ eRecruitment/src/main/java/com/t404notfound/erecruitment/controller/ManagerAcceptDecline.java
@@ -6,6 +6,7 @@ package com.t404notfound.erecruitment.controller;
 
 import com.t404notfound.erecruitment.bean.AdminUserDAO;
 import com.t404notfound.erecruitment.bean.AdminUserDTO;
+import com.t404notfound.erecruitment.bean.UserDTO;
 import com.t404notfound.erecruitment.bean.interview.ManagerParticipantDAO;
 import com.t404notfound.erecruitment.bean.interview.ManagerParticipantDTO;
 import java.io.IOException;
@@ -19,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -41,6 +43,13 @@ public class ManagerAcceptDecline extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         String url = "./ManagerViewCandidates";
+        HttpSession session = request.getSession();
+        UserDTO user = (UserDTO) session.getAttribute("user");
+        if (user == null) {
+            response.sendRedirect("login");
+        } else if (user.getUserRole() != 3) {
+            response.sendRedirect("home");
+        }
         try {
             String SearchValue = request.getParameter("SearchValue").trim();
             request.setAttribute("SearchValue", SearchValue);
